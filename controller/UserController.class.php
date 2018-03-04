5425ffc952736cc6efd686d1a5b7df3ee6070d71
SergeySaenko <svsaenko@inbox.ru>
2018-03-04 19:03:41 +0300

    broke auth))

--- controller/UserController.class.php
+++ controller/UserController.class.php
@@ -1,11 +1,14 @@
 <?php
+/*
+* Контроллер функций пользователя
+*/
 
 class UserController extends Controller
 {
   public $view = 'user';
   public $title;
-  public $msg = '';
-  public $form;// = new LoginForm($_POST);
+  public $msg;
+  public $form;
 
 	function __construct()
     {
@@ -20,19 +23,57 @@ class UserController extends Controller
 	        $username = $form->getUsername();
 	        $password = new Password( $form->getPassword() );
 
-	        $res = User::loginUser($form);
+	        $res = User::loginUser($username, $password);
 	        if (!$res) {
-	            $msg = 'No such user found';
+	            return [ 'msg' => 'No such user found'];
 	        } else {
 	            $user = $res[0]['username'];
 	            Session::set('user', $user);
-	            $msg = 'You have been logged in';
+	            return [ 'msg' => 'You have been logged in'];
 	            //header('location: index.php?msg=You have been logged in');
 	        }
 
 	    } else {
-	        $msg = 'Please fill in fields';
+	        return [ 'msg' => 'Please fill in fields'];
 	    }
+	}
+
+	public function register(){
+		$email = isset($_REQUEST['email']) ? $_REQUEST['email'] : null;
+		$email = trim($email);
+		$pwd1 = isset($_REQUEST['pwd1']) ? $_REQUEST['pwd1'] : null;
+		$pwd2 = isset($_REQUEST['pwd2']) ? $_REQUEST['pwd2'] : null;
+		$phone = isset($_REQUEST['phone']) ? $_REQUEST['phone'] : null;
+		$address = isset($_REQUEST['address']) ? $_REQUEST['address'] : null;
+		$name = isset($_REQUEST['name']) ? $_REQUEST['name'] : null;
+		$name = trim($name);
+
+		$resData = null;
+		$resData = User::checkRegisterParams($email, $pwd1, $pwd2);	
+
+		if(!$resData && User::checkUserEmail($email)){
+			$resData['success'] = false;
+			$resData['message'] = "Пользователь с таким ('{$email}') уже зарегистрирован";
+		}
+		if (!$resData) {
+			$pwdMD5 = md5($pwd1);
+			$userData = User::registerNewUser($email, $pwdMD5, $name, $phone, $address);
+			if ($userData['success']) {
+				$resData['message'] = "Пользователь успешно зарегистрирован";
+				$resData['success'] = 1;
+
+				$userData = $userData[0];
+				$resData['userName'] = $userData['name'] ? $userData['name'] : $userData['email'];
+				$resData['email'] = $email;
+
+				$_SESSION['user'] = $userData;
+				$_SESSION['user']['displayName'] = $userData['name'] ? $userData['name'] : $userData['email'];
+			} else {
+				$resData['success'] = 0;
+				$resData['message'] = "Ошибка регистрации";
+			}
+		}
+		echo json_encode($resData);
 	}
 	
 }
--- model/User.class.php
+++ model/User.class.php
@@ -1,5 +1,9 @@
 <?php 
 
+/*
+* Модель для работы с таблицей users
+*/
+
 class User extends Model {
   protected static $table = 'users';
 
@@ -30,7 +34,84 @@ class User extends Model {
     return db::getInstance()->Select(
       'SELECT * FROM users WHERE user_login=:username AND user_password = :password LIMIT 1',
       ['username' => $username, 'password' => $password]);
+  }
+
+  /*
+  * Регистрация нового пользователя
+  * 
+  * @param string $email
+  * @param string $pwdMD5
+  * @param string $name
+  * @param string $phone
+  * @param string $address
+  * @return array users data
+  */
+  public static function registerNewUser($email, $pwdMD5, $name, $phone, $address)
+  {
+    $email = htmlspecialchars(mysql_real_escape_string($email));//заменить на PDO
+    $name = htmlspecialchars(mysql_real_escape_string($name));
+    $phone = htmlspecialchars(mysql_real_escape_string($phone));
+    $address = htmlspecialchars(mysql_real_escape_string($address));
+
+    $sql = "INSERT INTO 
+              users (`email`, `pwd`, `name`, `phone`, `address`)
+              VALUES ('{$email}', '{$pwdMD5}', '{$name}', '{$phone}', '{$address}')";
+    $rs = mysql_query($sql);
+    
+    if($rs){
+      $sql = "SELECT * FROM users WHERE ( `email` = '{$email}' and `pwd` = '{$pwdMD5}') LIMIT 1";
+      $rs = mysql_query($sql);
+      $rs = createSmartyRsArray($rs);
+
+      if(isset($rs[0])){
+        $rs['success'] = 1;
+      } else {
+        $rs['success'] = 0;
+      }
+    } else {
+        $rs['success'] = 0;
     }
 
+    return $rs;
+  }
+
+  /*
+  * Проверка параметров регистрации пользователя
+  */
+  public static function checkRegisterParams($email, $pwd1, $pwd2)
+  {
+    $res = null;
+
+    if(!$email){
+      $res['success'] = false;
+      $res['message'] = 'Введите email';
+    }    
+
+    if(!$pwd1){
+      $res['success'] = false;
+      $res['message'] = 'Введите пароль';
+    }
+
+    if(!$pwd2){
+      $res['success'] = false;
+      $res['message'] = 'Введите повтор пароля';
+    }
+  
+    if($pwd1 != $pwd2){
+      $res['success'] = false;
+      $res['message'] = 'Пароли не совпадают';
+    }
+  return $res;
+  }
+
+  public static function checkUserEmail($email)
+  {
+    $email = htmlspecialchars(mysql_real_escape_string($email));
+    $sql = "SELECT id FROM users WHERE email = '{$email}'";
+    $rs = mysql_query($sql);
+    $rs = createSmartyRsArray($rs);
+
+    return $rs;
+  }
 }
 ?>
