021e663fdfbce337da3a13fab393b306ec234fdc
SergeySaenko <svsaenko@inbox.ru>
2018-03-06 21:43:35 +0300

    broke auth)) (reverted from commit 5425ffc952736cc6efd686d1a5b7df3ee6070d71)

--- controller/UserController.class.php
+++ controller/UserController.class.php
@@ -1,14 +1,11 @@
 <?php
-/*
-* Контроллер функций пользователя
-*/
 
 class UserController extends Controller
 {
   public $view = 'user';
   public $title;
-  public $msg;
-  public $form;
+  public $msg = '';
+  public $form;// = new LoginForm($_POST);
 
 	function __construct()
     {
@@ -23,57 +20,19 @@ class UserController extends Controller
 	        $username = $form->getUsername();
 	        $password = new Password( $form->getPassword() );
 
-	        $res = User::loginUser($username, $password);
+	        $res = User::loginUser($form);
 	        if (!$res) {
-	            return [ 'msg' => 'No such user found'];
+	            $msg = 'No such user found';
 	        } else {
 	            $user = $res[0]['username'];
 	            Session::set('user', $user);
-	            return [ 'msg' => 'You have been logged in'];
+	            $msg = 'You have been logged in';
 	            //header('location: index.php?msg=You have been logged in');
 	        }
 
 	    } else {
-	        return [ 'msg' => 'Please fill in fields'];
+	        $msg = 'Please fill in fields';
 	    }
-	}
-
-	public function register(){
-		$email = isset($_REQUEST['email']) ? $_REQUEST['email'] : null;
-		$email = trim($email);
-		$pwd1 = isset($_REQUEST['pwd1']) ? $_REQUEST['pwd1'] : null;
-		$pwd2 = isset($_REQUEST['pwd2']) ? $_REQUEST['pwd2'] : null;
-		$phone = isset($_REQUEST['phone']) ? $_REQUEST['phone'] : null;
-		$address = isset($_REQUEST['address']) ? $_REQUEST['address'] : null;
-		$name = isset($_REQUEST['name']) ? $_REQUEST['name'] : null;
-		$name = trim($name);
-
-		$resData = null;
-		$resData = User::checkRegisterParams($email, $pwd1, $pwd2);	
-
-		if(!$resData && User::checkUserEmail($email)){
-			$resData['success'] = false;
-			$resData['message'] = "Пользователь с таким ('{$email}') уже зарегистрирован";
-		}
-		if (!$resData) {
-			$pwdMD5 = md5($pwd1);
-			$userData = User::registerNewUser($email, $pwdMD5, $name, $phone, $address);
-			if ($userData['success']) {
-				$resData['message'] = "Пользователь успешно зарегистрирован";
-				$resData['success'] = 1;
-
-				$userData = $userData[0];
-				$resData['userName'] = $userData['name'] ? $userData['name'] : $userData['email'];
-				$resData['email'] = $email;
-
-				$_SESSION['user'] = $userData;
-				$_SESSION['user']['displayName'] = $userData['name'] ? $userData['name'] : $userData['email'];
-			} else {
-				$resData['success'] = 0;
-				$resData['message'] = "Ошибка регистрации";
-			}
-		}
-		echo json_encode($resData);
 	}
 	
 }
--- public/js/main.js
+++ public/js/main.js
@@ -14,41 +14,4 @@ $(document).ready(function(){
       dataType : "text"
     })
   });
-
-  /*
-  * Получение данных из формы
-  */
-  function getData(obj_form){
-    var hData = {};
-    $('input, textarea, select', obj_form).each(function(){
-      if(this.name && this.name != ''){
-        hData[this.name] = this.value;
-        console.log('hData[' + this.name + '] = ' + hData[this.name]);
-      }
-    });
-    return hData;
-  }
-
-
-
-  $('#registerMe').on('click', function(){
-    var postData = getData('#registerBox');
-    $.ajax({
-      type: 'POST',
-      //async: false,
-      url: "index.php?path=user/register/",
-      data: postData,
-      dataType: 'json',
-      error: function() {alert("Что-то пошло не так...");},
-      success: function(data){
-        if(data['success']){
-          alert(data['message']);
-
-          $('#registerBox').hide();
-        } else {
-          alert(data['message']);
-        }
-      }
-    })
-  })
 });
